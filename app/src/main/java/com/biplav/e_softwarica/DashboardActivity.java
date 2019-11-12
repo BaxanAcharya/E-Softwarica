e63d3e5fef40d4d2da04d97c745bc591f0b9d673
BaxanAcharya <baxanacharya@gmail.com>
2019-11-12 22:28:08 +0545

    'added listener to the bottom navigation to show fragment'

--- app/src/main/java/com/biplav/e_softwarica/DashboardActivity.java
+++ app/src/main/java/com/biplav/e_softwarica/DashboardActivity.java
@@ -1,8 +1,16 @@
 package com.biplav.e_softwarica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.biplav.e_softwarica.fragment.AboutUsFragment;
+import com.biplav.e_softwarica.fragment.AddStudentFragment;
+import com.biplav.e_softwarica.fragment.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +18,32 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.itemhome:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.itemadd:
+                            selectedFragment = new AddStudentFragment();
+                            break;
+                        case R.id.itemus:
+                            selectedFragment = new AboutUsFragment();
+                            break;
+                    }
+
+                   getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+
+                }
+            };
 }
